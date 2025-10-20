b383982a4882cf0a251b9f5069e1915b19df9efc
IsseiHasegawa <hasegawa01@allegheny.edu>
2025-10-20 16:59:37 -0400

    Create hello.html and templates

--- introFlask/flaskBasic/hello.py
+++ introFlask/flaskBasic/hello.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/japan/<city>")
-def japan(city):
-    return f"Hello, {city} in Japan!"
+@app.route("/")
+def hello():
+    return render_template("hello.html")
 
